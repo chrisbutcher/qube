4daad4c1b7f1e00fcb6ad976f13e368bd480168f
chris butcher <cbutcher@gmail.com>
2019-09-01 22:20:02 -0400

    detonated marker fixes

--- Assets/Scripts/MarkerType.cs
+++ Assets/Scripts/MarkerType.cs
@@ -15,6 +15,13 @@ public class MarkerType : MonoBehaviour {
   public Type CurrentType;
 
   public static void SpawnDestroyedMarkerAt(Vector3 spawnPosition) {
+    var playerMarkers = new List<GameObject>(GameObject.FindGameObjectsWithTag("PlayerMarker"));
+    var exitingDetonatedMarkerAtThisPosition = playerMarkers.Exists(m => m.GetComponent<MarkerType>().CurrentType != MarkerType.Type.DestroyedMarker && m.transform.position == spawnPosition);
+    if (exitingDetonatedMarkerAtThisPosition) {
+      // Return early if there is already a detonated marker in this spot.
+      return;
+    }
+
     var prefab = GameManager.instance.Players[0].GetComponent<PlayerMarker>().PlayerMarkerPrefab;
     var destroyedPlayerMarker = (GameObject)Instantiate(prefab, spawnPosition, Quaternion.identity);
     destroyedPlayerMarker.GetComponent<MarkerType>().CurrentType = MarkerType.Type.DestroyedMarker;
